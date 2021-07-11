8f69b78d90d17f9143aa6ad9d8408d016a7764be
LRLVEC <mjdyx@outlook.com>
2021-07-11 23:14:36 +0800

    two player, two window

--- HexPong/shaders/PlayerVertex0.cpp
+++ HexPong/shaders/PlayerVertex0.cpp
@@ -1,14 +1,17 @@
 #version 450 core
 layout(location = 0) in vec2 position;
 //layout(location = 1) in vec3 color;
-layout(std140, row_major, binding = 1)uniform OffsetBuffer
+layout(std140, row_major, binding = 0)uniform OffsetBuffer
 {
 	vec2 offsets[6];
+	uint inversed;
 };
 //flat out vec4 in_color;
 void main()
 {
 	unsigned int id = gl_VertexID / 6;
-	gl_Position = vec4(position + offsets[id], 0, 1);
+	vec2 temp = position + offsets[id];
+	if (inversed == 0)gl_Position = vec4(temp, 0, 1);
+	else gl_Position = vec4(-temp, 0, 1);
 	//in_color = vec4(color, 1);
 }
